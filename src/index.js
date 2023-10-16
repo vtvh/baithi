85afca105cad2a33de47fc612037552c52a30d55
Vu Tran Viet Hai <andreehai1011@gmail.com>
2023-10-16 18:19:26 +0700

    install and add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
